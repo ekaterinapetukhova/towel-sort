216870c025cd3423ecd5903cea1bc39fc4539b79
ekaterinapetukhova <katepthv12@gmail.com>
2023-08-13 22:03:49 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
+var _ = require("lodash");
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (typeof matrix === "object") {
+        for (let i = 0; i < matrix.length; i++) {
+            let arr = matrix[i];
+
+            if (matrix.indexOf(arr) % 2 !== 0) {
+                arr = arr.reverse();
+            }
+
+            matrix.splice(i, 1, arr);
+        }
+
+        matrix = _.flattenDeep(matrix);
+    } else {
+        matrix = [];
+    }
+    return matrix;
+};
